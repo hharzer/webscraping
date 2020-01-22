c54debaa18979efb2397619f23508b6cf19251cc
Ennaim Yassine <ennaimyassine@gmail.com>
2020-01-22 21:26:54 +0100

    Profile api's

--- api/profile/controllers/profile.js
+++ api/profile/controllers/profile.js
@@ -5,4 +5,47 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async findOne (ctx) {
+        const {id} = ctx.params;
+        return strapi.query('profile').model
+        .findById(id)
+        .populate({
+            path: 'user',
+            select: 'username email id'
+        })
+    },
+    async update (ctx) {
+        const {id} = ctx.params;
+        const {likes, downloads, picture} = ctx.request.body;
+        const user = await strapi.query('profile').model.findById(id);
+        let objUpdate = {}
+        if(user) {
+            if(likes) {
+                const lkIndex = user.likes.indexOf(likes);
+                console.log('lkIndex',lkIndex);
+                if (lkIndex > -1)
+                    user.likes.splice(lkIndex, 1)
+                else
+                    user.likes.push(likes);
+                objUpdate.likes = user.likes;
+            }
+            if (downloads) {
+                const dwIndex = user.downloads.indexOf(downloads);
+                if (dwIndex > -1) 
+                    user.downloads.splice(dwIndex, 1)
+                else 
+                    user.downloads.push(downloads);
+                objUpdate.downloads = user.downloads;
+            }
+            if (picture) {
+                objUpdate.picture = picture;
+            }
+
+        }
+        console.log(objUpdate)
+        return strapi.query('profile').update({id},objUpdate)
+        .then(r => ctx.send({"done": true}))
+        .catch(e => ctx.send({"error": true}))
+    }
+};
